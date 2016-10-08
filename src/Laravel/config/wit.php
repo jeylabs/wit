216f793e0fca84d57aa425ab90e68cd20a4b34f7
Sureshkumar Selvanayagam <sureshkumaronnet@gmail.com>
2016-10-08 08:47:51 +0000

    Applied fixes from StyleCI

--- src/Laravel/config/wit.php
+++ src/Laravel/config/wit.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'access_token' => env('WIT_ACCESS_TOKEN', 'YOUR_WIT_ACCESS_TOKEN'),
+    'access_token'   => env('WIT_ACCESS_TOKEN', 'YOUR_WIT_ACCESS_TOKEN'),
     'async_requests' => env('WIT_ASYNC_REQUESTS', false),
 ];
